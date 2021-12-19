f865b78d02c4d57c38f3c13ef50c8da4bf678388
Alexey Prokopov <prokopov.alexey@gmail.com>
2021-12-20 00:00:02 +0300

    user/stat console command

--- commands/UserController.php
+++ commands/UserController.php
@@ -29,5 +29,25 @@ class UserController extends Controller {
         
         return ExitCode::OK;
     }
+
+    public function actionStat() {
+        
+        /* @var $db \yii\db\Connection */
+        $db = \Yii::$app->db;
+        
+        $totalUsersWithSantaCount = $db->createCommand('SELECT COUNT(*) FROM `user`')->queryScalar();
+        $usersWithSantaCount = $db->createCommand('SELECT COUNT(*) FROM `user` WHERE santa_id IS NOT NULL')->queryScalar();
+        $usersChosenByThemselves = $db->createCommand('SELECT COUNT(*) FROM `user` WHERE santa_id IS NOT NULL AND santa_id = id')->queryScalar();
+        $usersCreatedWishlists = $db->createCommand('SELECT COUNT(*) FROM `user` WHERE wishlist IS NOT NULL')->queryScalar();
+        $usersWithSameSanta = $db->createCommand('SELECT COUNT(*) FROM `user` u1 INNER JOIN `user` u2 ON u1.santa_id IS NOT NULL AND u2.santa_id IS NOT NULL AND u2.santa_id=u1.santa_id WHERE u1.id <> u2.id')->queryScalar();
+
+        echo "Total users: $totalUsersWithSantaCount\n";
+        echo "Users chosen by Santa: $usersWithSantaCount\n";
+        echo "Users, created wishlist: $usersCreatedWishlists\n";
+        echo "Users, chosen by themselves: $usersChosenByThemselves\n";
+        echo "Users, chosen by the same Santa: $usersWithSameSanta\n";
+        
+        return ExitCode::OK;
+    }
     
 }
